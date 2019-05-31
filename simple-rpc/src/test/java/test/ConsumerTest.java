0e03c5ee7e0887cdb34b38b80d2b4e14b73482a2
dragonhht <dragon_hht@163.com>
2019-05-31 17:16:04 +0800

    日志设置

--- simple-rpc/src/test/java/test/ConsumerTest.java
+++ simple-rpc/src/test/java/test/ConsumerTest.java
@@ -3,7 +3,10 @@ package test;
 import com.github.dragonhht.rpc.api.HelloService;
 import com.github.dragonhht.rpc.common.CommonConstants;
 import com.github.dragonhht.rpc.consumer.ProxyHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.util.Scanner;
 
 /**
  * .
@@ -11,13 +14,25 @@ import org.junit.Test;
  * @author: huang
  * @Date: 2019-5-31
  */
+@Slf4j
 public class ConsumerTest {
 
     @Test
     public void testCall() {
         HelloService service = (HelloService) ProxyHandler.newInstance(null, CommonConstants.PROVIDER_PORT,
                 CommonConstants.HOST, CommonConstants.REGISTRY_PORT, HelloService.class);
-        System.out.println(service.sayHello("drag"));
+        System.out.println(service.sayHello("world"));
+    }
+
+    public static void main(String[] args) {
+        HelloService service = (HelloService) ProxyHandler.newInstance(null, CommonConstants.PROVIDER_PORT,
+                CommonConstants.HOST, CommonConstants.REGISTRY_PORT, HelloService.class);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------------请输入---------------");
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            System.out.println(service.sayHello(str));
+        }
     }
 
 }
